d20e87b28cb95c6c46644b5b351f1d56a06659f9
Silicrex <silicrex@gmail.com>
2021-07-05 01:57:57 -0400

    Bot can run; moved appropriate functions to console_interaction

--- main.py
+++ main.py
@@ -1,49 +1,12 @@
 import discord
 from discord.ext import commands
-import os
-import json
-
-
-def get_console_confirmation(question):
-    # Adds " (y/n)" text. Does not add the question mark.
-    # example question input: 'Generate the file?'
-    while True:  # Repeat until 'y'/'yes' or 'n'/'no' are given
-        print(question + ' (y/n)')
-        console_response = input().lower()
-        if console_response in {'y', 'yes'}:
-            return True
-        elif console_response in {'n', 'no'}:
-            return False
-
-
-def get_bot_token():
-    if os.path.isfile('token.json'):  # token.json exists, attempt to read the token from it
-        with open('token.json') as file:
-            try:
-                token = json.load(file)  # If successful, then return token
-                return token
-            except json.JSONDecodeError:
-                print('token.json could not parsed. Is it formatted correctly? '
-                      'Make sure it has your token as a double-quoted string, then relaunch.')
-                input()  # Pause to prevent console from closing instantly if opened from script
-                quit()
-    else:  # token.json file does not exist in directory
-        print('ERROR: Could not find token.json file')
-        if not get_console_confirmation('Should I generate it?'):  # Offer to generate token.json in directory
-            quit()
-        with open('token.json', 'w') as file:  # Confirmed; proceed to generate file
-            print()  # Newline
-            print('Generated token.json. It should contain your bot token as a double-quote string.')
-            if get_console_confirmation('Should I write the token to it?'):
-                print()  # Newline
-                print('Enter your token, without quotes:')  # Confirmed; ask for token
-                token = input()
-                json.dump(token, file)
-                return token
-            else:
-                quit()  # If token is not given, it must be manually filled in before proceeding
+import console_interaction
 
 
 bot = commands.Bot(command_prefix='.')
-token = get_bot_token()
-print(token)
+token = console_interaction.get_bot_token()
+try:
+    bot.run(token)
+except discord.LoginFailure:
+    print('Login failed. Is the token valid?')
+    input()  # Pause
