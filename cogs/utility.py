303377b0bdeb335a6dc7ecb9558649f3992b083d
Silicrex <silicrex@gmail.com>
2021-07-05 14:17:11 -0400

    Add ping command

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 
 class Utility(commands.Cog):
@@ -9,6 +10,13 @@ class Utility(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f'{self.bot.user} ready')
+
+    @commands.command()
+    async def ping(self, ctx):
+        start_time = time.monotonic()
+        sent_message = await ctx.send('Pong!')
+        time_difference = time.monotonic() - start_time
+        await sent_message.edit(content='Pong! {:.0f} ms'.format(time_difference * 1000))
 
 
 def setup(bot):
