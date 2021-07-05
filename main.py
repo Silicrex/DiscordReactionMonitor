6e1aff5c7515c459db23ce2a439bbb5400404999
Silicrex <silicrex@gmail.com>
2021-07-05 13:41:36 -0400

    Load all cogs on startup

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import discord
 from discord.ext import commands
+import os
 import console_interaction
 
 
 bot = commands.Bot(command_prefix='.')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
 token = console_interaction.get_bot_token()
 try:
     bot.run(token)
