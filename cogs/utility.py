af1fbde9e9e923e0b7d9379f7f85b5c2b2824c65
Silicrex <silicrex@gmail.com>
2021-07-05 14:07:43 -0400

    Add on_ready event listener

--- cogs/utility.py
+++ cogs/utility.py
@@ -6,6 +6,10 @@ class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.bot.user} ready')
+
 
 def setup(bot):
     bot.add_cog(Utility(bot))
