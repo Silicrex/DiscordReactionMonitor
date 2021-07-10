2f682500ed92bccd9cbbd13afd336c95ffc377fc
Silicrex <silicrex@gmail.com>
2021-07-09 23:26:16 -0400

    Added cog load commands

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ import console_interaction
 
 bot = commands.Bot(command_prefix='.')
 
+
+@bot.command()
+@commands.is_owner()  # Owner-only command
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Loaded {extension}')
+
+
+@bot.command()
+@commands.is_owner()  # Owner-only command
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'Unloaded {extension}')
+
+
+@bot.command()
+@commands.is_owner()  # Owner-only command
+async def reload(ctx, extension):
+    bot.reload_extension(f'cogs.{extension}')
+    await ctx.send(f'Reloaded {extension}')
+
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
